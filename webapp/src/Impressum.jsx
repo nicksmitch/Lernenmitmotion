7eb097678d9b983b8acf97037fc1b148c3d397c8
nicksmitch <nick9mitchell7@googlemail.com>
2026-01-16 17:08:56 +0100

    Add Impressum component with contact information

---
+++ webapp/src/Impressum.jsx
@@ -0,0 +1,24 @@
+export default function Impressum() {
+  return (
+    <div style={{ padding: "2rem", maxWidth: "800px" }}>
+      <h1>Impressum</h1>
+
+      <p>Angaben gemäß § 5 TMG</p>
+
+      <p>
+        Nick Mitchell<br />
+        Weissenseer Weg 12, 10367 Berlin<br />
+        Deutschland
+      </p>
+
+      <p>
+        E-Mail: nick9mitchell7@googlemail.com
+      </p>
+
+      <p>
+        Privates Lern- und Entwicklungsprojekt.
+      </p>
+    </div>
+  );
+}
+
